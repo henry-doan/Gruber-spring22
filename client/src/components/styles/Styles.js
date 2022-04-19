879a92be470891d5b924fad4415f1b6db2395f11
Sean Patrick Parnell <seanpatrickparnell@gmail.com>
2022-04-18 19:15:17 -0600

    bigforma

--- client/src/components/styles/Styles.js
+++ client/src/components/styles/Styles.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import {Col} from 'react-bootstrap';
+import {Col, Row, Container, Button} from 'react-bootstrap';
 
 const fontSize = (size) => {
   switch(size) {
@@ -40,3 +40,50 @@ export const MainHeader = styled.div`
 export const MainHeaderText = styled(Col)`
   margin: auto;
 `
+
+export const SBSRow = styled(Row)`
+ margin-bottom: 20px;
+ margin-top: 20px;
+ border-bottom: 1px solid #f3f3f3;
+ 
+
+ 
+ 
+ 
+  
+  `
+
+  export const SBSh3 = styled.h3`
+  text-align: left;
+  margin-bottom: 50px;
+  border-bottom: 5px;
+
+  
+  `
+  export const SBSContainer = styled(Container)`
+  width: 50%;
+  margin: 0 auto;
+  border-bottom: 5px;
+  
+  
+  `
+
+  export const SBSh4 = styled.h4`
+  
+  
+  
+
+
+  `
+  export const SBSimg = styled.img`
+  margin-bottom: 20px;
+  width: 200px;
+
+`
+export const SBSButton = styled(Button)`
+margin-left: 80%;
+  margin-bottom: 20px;
+  width: 100px;
+  
+
+  `
