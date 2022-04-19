d6486890e7cf438a83e339f3c5b624b7278ff329
Austen Wilde <sawilde15@gmail.com>
2022-04-18 18:20:28 -0600

    austen_nav

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -1,5 +1,42 @@
+import { MainHeader, MainHeaderText, SubHeader} from '../styles/Styles';
+import {Row, Nav, Button, Col, Container, Accordion } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
 const Home = () => (
-  <h3>Welcome to Gruber</h3>
+  <>
+    <MainHeader>
+      <Row>
+        <Container>
+        <MainHeaderText>
+          <h1>Gruber</h1>
+          <p>Movement is what we power. It’s our lifeblood. It runs 
+            through our veins. It’s what gets us out of bed each 
+            morning. It pushes us to constantly reimagine how we can 
+            move better. For you. For all the places you want to go. 
+            For all the things you want to get. For all the ways you 
+            want to earn. Across the entire world. In real time. At the 
+            incredible speed of now.</p>
+        </MainHeaderText>
+        </Container>
+          <Nav.Link>
+            <Link to='/login'>
+              <Button>
+                Log In
+                </Button>
+            </Link>
+          {/* </Nav.Link>
+          <Nav.Link> */}
+            <Link to='/register'>
+              <Button>
+                Get Started
+              </Button>
+            </Link>
+          </Nav.Link>
+        
+      </Row>
+    
+    </MainHeader>
+  </>
 )
 
 export default Home;
--- client/src/components/shared/MainNavbar.js
+++ client/src/components/shared/MainNavbar.js
@@ -1,6 +1,6 @@
 import { AuthConsumer } from "../../providers/AuthProvider";
 import { Link } from 'react-router-dom';
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container, Button} from 'react-bootstrap';
 
 const MainNavbar = ({user, handleLogout }) => {
   
@@ -33,30 +33,47 @@ const MainNavbar = ({user, handleLogout }) => {
       return (
         <>
           <Nav.Link>
-            <Link to='/login'>
+            <Link to='/register'>
               <li>
-                Login
+                Sign Up
               </li>
             </Link>
           </Nav.Link>
           <Nav.Link>
-            <Link to='/register'>
-              <li>
-                Register
-              </li>
+            <Link to='/login'>
+              {/* <li>
+                Login
+              </li> */}
+              <Button>Login</Button>
             </Link>
           </Nav.Link>
         </>
       )
     }
   }
+
   
   // links that show up regardless of login or out
   return (
     <>
      <Navbar collapseOnSelect expand="lg">
         <Container>
-          <Link to='/'><Navbar.Brand>Gruber</Navbar.Brand></Link>
+          <Nav className="justify-content-left">
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Menu One</Nav.Link>
+            </Nav.Item>
+             <Nav.Item>
+              <Nav.Link eventKey="link-2">Menu Two</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-3">Menu Three</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Nav className="justify-content-center">
+            <Link to='/'>
+              <Navbar.Brand>Gruber</Navbar.Brand>
+            </Link>
+          </Nav>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="me-auto">
@@ -71,6 +88,7 @@ const MainNavbar = ({user, handleLogout }) => {
     </>
   )
 }
+
 
 const ConnectedMainNavbar = (props) => (
   <AuthConsumer> 
