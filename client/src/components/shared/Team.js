9fc23422f34403d5596b35ceec3036af425a851f
Brandon Shay <brandonshayproductions@gmail.com>
2022-04-28 19:29:21 -0600

    prod

--- client/src/components/shared/Team.js
+++ client/src/components/shared/Team.js
@@ -35,8 +35,7 @@ const Team2 = () => (
               <Card.Body>
                 <Card.Title>Brandon Shay</Card.Title>
                 <Card.Text>
-                  Some quick example text to build on the card title and make up the bulk of
-                  the card's content.
+                  Mowing my way to Web Development
                 </Card.Text>
           
               </Card.Body>
